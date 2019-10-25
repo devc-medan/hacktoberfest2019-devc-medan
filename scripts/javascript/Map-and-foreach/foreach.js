8399121312549af8167277e54cf0d2545a7f8da4
Amril Syaifa Yasin <38855488+amrilsyaifa@users.noreply.github.com>
2019-10-25 14:29:00 +0700

    Amril (#37)
    * object keys
    
    * Map javascript
    
    * Foreach Javascript
    
    * Readme

---
+++ scripts/javascript/Map-and-foreach/foreach.js
@@ -0,0 +1,17 @@
+// Data yang kita punya
+var officers = [
+	{ id: 20, name: 'Captain Piett' },
+	{ id: 24, name: 'General Veers' },
+	{ id: 56, name: 'Admiral Ozzel' },
+	{ id: 88, name: 'Commander Jerjerrod' }
+];
+
+// yang kita ingin dapatkan
+//   [20, 24, 56, 88]
+
+var officersIds = [];
+officers.forEach(function(officer) {
+	officersIds.push(officer.id);
+});
+
+console.log(officersIds); //[20, 24, 56, 88]
